8f1e951687105241fb63d6f145b6fb15899666e5
l1quan268 <tranhongquan26082005@gmail.com>
2025-04-12 14:35:53 +0700

    giao diện lanspy

---
+++ Alerts.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LANSPYproject
+{
+    /// <summary>
+    /// Interaction logic for Alerts.xaml
+    /// </summary>
+    public partial class Alerts : Window
+    {
+        public class AlertItem
+        {
+            public string ThoiGian { get; set; }
+            public string Loai { get; set; }
+            public string NoiDung { get; set; }
+        }
+        public Alerts()
+        {
+            InitializeComponent();
+            AlertDataGrid.ItemsSource = new List<AlertItem>
+        {
+            new AlertItem { ThoiGian = "11:22:10 10/04/2025", Loai = "Thông thường", NoiDung = "Thiết bị mới: 192.168.1.105" },
+            new AlertItem { ThoiGian = "11:24:03 10/04/2025", Loai = "Lỗi nghiêm trọng", NoiDung = "Mất kết nối: 192.168.1.107" },
+            new AlertItem { ThoiGian = "11:25:30 10/04/2025", Loai = "Thông thường", NoiDung = "Thiết bị lạ: MAC AA-BB-CC-DD-EE-FF" },
+            new AlertItem { ThoiGian = "11:26:45 10/04/2025", Loai = "Lỗi nghiêm trọng", NoiDung = "Không ping được: 192.168.1.110" },
+            new AlertItem { ThoiGian = "11:28:12 10/04/2025", Loai = "Thông thường", NoiDung = "Quá tải: 52 thiết bị hoạt động" },
+        };
+        }
+    }
+
+}
---
+++ Scanner.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LANSPYproject
+{
+    /// <summary>
+    /// Interaction logic for Scanner.xaml
+    /// </summary>
+    public partial class Scanner : UserControl
+    {
+        public class Device
+        {
+            public int ID { get; set; }
+            public string IP { get; set; }
+            public string MAC { get; set; }
+            public string Name { get; set; }
+            // Date sẽ luôn được set theo thời gian hiện tại khi tạo mới
+            public string Date { get; set; } = DateTime.Now.ToString("dd/MM, hh:mm tt");
+        }
+
+        public Scanner()
+        {
+            InitializeComponent();
+
+            List<Device> devices = new List<Device>
+        {
+            new Device { ID = 1, IP = "192.168.1.10", MAC = "2C:54:91:88:C9:E3", Name = "hp el g4", Date = "25/03, 10:40 PM" },
+            new Device { ID = 2, IP = "192.168.1.8", MAC = "D0:38:DC:3C:5B:48", Name = "Thinkpad x240", Date = "25/03, 10:40 PM" }
+        };
+            
+            //deviceDataGrid.ItemsSource = devices;
+        }
+    }
+
+}
